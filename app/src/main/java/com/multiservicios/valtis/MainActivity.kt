27bb5fb91e43994c9af434881f8a522759a08356
isai1703 <oscar_isai_170596@hotmail.com>
2026-09-06 02:45:56 -0600

    Fix Compose Composable import

--- app/src/main/java/com/multiservicios/valtis/MainActivity.kt
+++ app/src/main/java/com/multiservicios/valtis/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
